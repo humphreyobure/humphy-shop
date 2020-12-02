dde859f74dabab467fed7dca3d21a5b83ba23339
Humphrey Tom <humphreytom14@gmail.com>
2020-12-02 19:47:45 +0300

    Updates

---
+++ precache-manifest.a9b72ed166838ad9d4bdb2534ecbca8c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521728ae9e75739969fef07caf78321d",
+    "url": "/humphy-shop/index.html"
+  },
+  {
+    "revision": "b99b210895a8f336a77b",
+    "url": "/humphy-shop/static/css/2.9c0f8afa.chunk.css"
+  },
+  {
+    "revision": "444469a5cf9dcf52f9d2",
+    "url": "/humphy-shop/static/css/main.74004fe1.chunk.css"
+  },
+  {
+    "revision": "b99b210895a8f336a77b",
+    "url": "/humphy-shop/static/js/2.c6ca94f8.chunk.js"
+  },
+  {
+    "revision": "1cbe9a2cbfc7bde61c2785ac25b62c38",
+    "url": "/humphy-shop/static/js/2.c6ca94f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444469a5cf9dcf52f9d2",
+    "url": "/humphy-shop/static/js/main.7c19023b.chunk.js"
+  },
+  {
+    "revision": "d0339b7d7e3954962fe4",
+    "url": "/humphy-shop/static/js/runtime-main.30682ebd.js"
+  },
+  {
+    "revision": "693a42b60ebfcb852f19d2b4546d4649",
+    "url": "/humphy-shop/static/media/analog-quartz-watch.693a42b6.jpg"
+  },
+  {
+    "revision": "3a23615a36cf83b355befb82f9d9b6e0",
+    "url": "/humphy-shop/static/media/baby_dress.3a23615a.jpg"
+  },
+  {
+    "revision": "001b76d1ddd3da02fd7748581a9944ee",
+    "url": "/humphy-shop/static/media/belt.001b76d1.jpg"
+  },
+  {
+    "revision": "2b586f68b033acf28bde3218e04982f5",
+    "url": "/humphy-shop/static/media/bodycon-dress.2b586f68.jpg"
+  },
+  {
+    "revision": "680cdf4d9b26e0bd5e5d8ba5d89d2ffb",
+    "url": "/humphy-shop/static/media/boy_boxers.680cdf4d.jpg"
+  },
+  {
+    "revision": "8eb21733cd052a372d360007ca97641c",
+    "url": "/humphy-shop/static/media/boys-t-shirt.8eb21733.jpg"
+  },
+  {
+    "revision": "fb08a8f9f88ef3491ca4969f9a12773b",
+    "url": "/humphy-shop/static/media/cart.fb08a8f9.svg"
+  },
+  {
+    "revision": "8fb0da77fdc15489af6dc4d01ab9f389",
+    "url": "/humphy-shop/static/media/check-circle.8fb0da77.png"
+  },
+  {
+    "revision": "04bdf262007df700b2941dafc48a61b2",
+    "url": "/humphy-shop/static/media/cotton-dress.04bdf262.jpg"
+  },
+  {
+    "revision": "f0904d0142910346593a3d7936e52c9b",
+    "url": "/humphy-shop/static/media/crew-neck-tshirt.f0904d01.jpg"
+  },
+  {
+    "revision": "eeb1b06c1da8f7116a86076256b24c93",
+    "url": "/humphy-shop/static/media/facebook-f.eeb1b06c.svg"
+  },
+  {
+    "revision": "9dab2fa2d8bb675c844207e8d8dce0c5",
+    "url": "/humphy-shop/static/media/fila_black.9dab2fa2.jpg"
+  },
+  {
+    "revision": "33f9c1135c87bb30dad483ac88552f3a",
+    "url": "/humphy-shop/static/media/flare_dress.33f9c113.png"
+  },
+  {
+    "revision": "ee1ddaee56b8c9b5658c60b75b000c24",
+    "url": "/humphy-shop/static/media/floral-dress.ee1ddaee.jpg"
+  },
+  {
+    "revision": "c5cfe541b8c905d09754565bdf435211",
+    "url": "/humphy-shop/static/media/gemch_shoes.c5cfe541.jpg"
+  },
+  {
+    "revision": "e099f125b5fee467e9ce8146d9277a8d",
+    "url": "/humphy-shop/static/media/gladiator-flat-flip.e099f125.jpg"
+  },
+  {
+    "revision": "0fd353be140c5cc34aaac1059749f8f4",
+    "url": "/humphy-shop/static/media/gsoft-khaki.0fd353be.jpg"
+  },
+  {
+    "revision": "a2218872cd9d9cb4765264388129a47f",
+    "url": "/humphy-shop/static/media/instagram.a2218872.svg"
+  },
+  {
+    "revision": "e3abd1e3fea3cdcdc94407ff86610860",
+    "url": "/humphy-shop/static/media/leather-shoes.e3abd1e3.jpg"
+  },
+  {
+    "revision": "40870f672ad272231c7409e37383273a",
+    "url": "/humphy-shop/static/media/linkedin.40870f67.svg"
+  },
+  {
+    "revision": "8c658eec7c1f183be39842f3a0005b81",
+    "url": "/humphy-shop/static/media/money.8c658eec.png"
+  },
+  {
+    "revision": "d4493484d2a043acad3980302f5e6e39",
+    "url": "/humphy-shop/static/media/princes-dress.d4493484.jpg"
+  },
+  {
+    "revision": "736a1d6f509010ac7eed81c3d20d4169",
+    "url": "/humphy-shop/static/media/quartz-wrist-watch.736a1d6f.jpg"
+  },
+  {
+    "revision": "e92ca21b8b8208b488d6d27dba7e2473",
+    "url": "/humphy-shop/static/media/shirt.e92ca21b.jpg"
+  },
+  {
+    "revision": "8c514601ed8845620c25d5802e5c893a",
+    "url": "/humphy-shop/static/media/singedani-handbag.8c514601.jpg"
+  },
+  {
+    "revision": "4a69a8e2535a030db0a407e9bdcd453f",
+    "url": "/humphy-shop/static/media/slim-fit-suit.4a69a8e2.jpg"
+  },
+  {
+    "revision": "dad7221edb8d601bb04a79eeda64ba63",
+    "url": "/humphy-shop/static/media/sneakers.dad7221e.jpg"
+  },
+  {
+    "revision": "602a6d685c9ac4912be41422843b117b",
+    "url": "/humphy-shop/static/media/truck.602a6d68.png"
+  },
+  {
+    "revision": "34f596da04cae2ccee955ac2aa2ba7a7",
+    "url": "/humphy-shop/static/media/twitter.34f596da.svg"
+  },
+  {
+    "revision": "3efbdcf213a2a99121a78bfc17fe8dc9",
+    "url": "/humphy-shop/static/media/vest.3efbdcf2.jpg"
+  },
+  {
+    "revision": "684af1763763b0707a78e2d52072338b",
+    "url": "/humphy-shop/static/media/vintage-flare-dress.684af176.jpg"
+  }
+]);
